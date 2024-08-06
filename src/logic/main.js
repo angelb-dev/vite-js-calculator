7f5607ee7032ff6c9b14369331806c68d3006047
Angel <angelbeneman801@gmail.com>
2024-08-06 14:43:45 -0300

    Updated

---
+++ src/logic/main.js
@@ -0,0 +1,20 @@
+import '../styles/style.css'
+
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.id === "="){
+            display.value = eval(display.value)
+        } else if (btn.id === "ac") {
+            display.value = ""
+        } else if (btn.id == "de") {
+            display.value = display.value.slice(0, -1)
+        } else {
+            display.value += btn.id
+        }
+    })
+})
+
+longtext ()
